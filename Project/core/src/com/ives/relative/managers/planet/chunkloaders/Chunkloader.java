9b0fcc7334d0f886d5253fe82994412afeca75fe
Ives van Hoorne <Ives.v.h@gmail.com>
2015-01-08 16:00:12 +0100

    Bugged file

---
+++ Project/core/src/com/ives/relative/managers/planet/chunkloaders/Chunkloader.java
@@ -0,0 +1,10 @@
+package com.ives.relative.managers.planet.chunkloaders;
+
+import com.ives.relative.managers.planet.Chunk;
+
+/**
+ * Created by Ives on 7/1/2015.
+ */
+public interface ChunkLoader {
+    public void loadChunkInfo(Chunk chunk);
+}
